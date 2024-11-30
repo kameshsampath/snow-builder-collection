e43c1e200d41e9a0156b56cd259a6ca015ac7397
Kamesh Sampath <kamesh.sampath@hotmail.com>
2024-11-30 09:42:10 +0530

    (fix): use path relative to script

--- forgeconfig/scripts/bin/docker-copy.sh
+++ forgeconfig/scripts/bin/docker-copy.sh
@@ -2,16 +2,22 @@
 
 set -euo pipefail 
 
-docker run -v apj-build-2024-slack-demo_snowflake_home:/snowflake busybox
+DOCKER_VOL_NAME=snow_forgeconfig
+CONTAINER_NAME="data_${DOCKER_VOL_NAME}"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DOCKER_COMPOSE_DIR="$(dirname $(dirname "$SCRIPT_DIR"))"
+DOCKER_COMPOSE_PROJECT_NAME=$(basename "$DOCKER_COMPOSE_DIR")
 
+docker run -v "${CONTAINER_NAME}":/snowflake busybox
 
 # Create a temporary container with the volume mounted
-docker container create --name data_snowflake_config -v  apj-build-2024-slack-demo_snowflake_home:/data busybox
+# the volume name if used docker compose will be "${PWD}_<volume_name>"
+docker container create --name "${CONTAINER_NAME}" -v  "${DOCKER_COMPOSE_PROJECT_NAME}_${DOCKER_VOL_NAME}":/data busybox
 
 # Copy files from the container to your local machine
-docker cp data_snowflake_config:/data .snowflake
+docker cp "${CONTAINER_NAME}":/data "${PWD}/.snowflake"
 
 # Remove the temporary container
-docker rm data_snowflake_config
+docker rm "${CONTAINER_NAME}"
 
-sed -i "s|/home/me|$PWD|g" .snowflake/config.toml
+sed -i "s|/home/me|$PWD|g" "${PWD}/.snowflake/config.toml"
