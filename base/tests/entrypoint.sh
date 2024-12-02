debda0d8e6795bcb91fd20230cafe856d7172144
Kamesh Sampath <kamesh.sampath@hotmail.com>
2024-12-02 23:12:27 +0530

    (fix): add gpg and logic to unset passphrase

---
+++ base/tests/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash 
+set -e
+set -o pipefail
+
+DECRYPT_CMD="gpg --quiet --batch --yes --decrypt --passphrase $PASSPHRASE /app/.env.gpg"
+
+# Export the decrypted environment variables
+eval "$($DECRYPT_CMD | sed 's/^/export /')"
+
+echo "Foo is $FOO"
+
+# do some process
+sleep .10
+# Unset variables after 10 seconds in the background
+eval "$($DECRYPT_CMD | cut -d= -f1 | sed 's/^/unset /')"
+
+sudo unset_passphrase && cat /etc/bash.bashrc 
+
+[ -n "${FOO}" ] && printf "\n Foo is %s" "$FOO" || printf "\nFOO is not set or unset"
